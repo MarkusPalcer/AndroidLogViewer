b919a04f593b1f8d4b95af2fadeefe4549c5a50c
Markus Palcer <Markus.Palcer@zuehlke.com>
2019-03-19 10:56:49 +0100

    Updated Assembly Information before

--- AndroidLogViewer/AndroidLogViewer/Properties/AssemblyInfo.cs
+++ AndroidLogViewer/AndroidLogViewer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AndroidLogViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Markus Palcer 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
