2d73f18922758fd983b28e7de06aa3d5ae03beed
Markus Palcer <Markus.Palcer@zuehlke.com>
2019-04-25 13:38:08 +0200

    Installer reads version from executable

--- AndroidLogViewer/AndroidLogViewer/Properties/AssemblyInfo.cs
+++ AndroidLogViewer/AndroidLogViewer/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AndroidLogViewer")]
+[assembly: AssemblyTitle("Android Log Viewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AndroidLogViewer")]
+[assembly: AssemblyCompany("Markus Palcer")]
+[assembly: AssemblyProduct("Android Log Viewer")]
 [assembly: AssemblyCopyright("Copyright © Markus Palcer 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
