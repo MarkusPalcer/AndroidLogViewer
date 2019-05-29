bd032b48e5c8765a48dc5c4290ea4d6bbda01a0f
Palcer, Markus <markus.palcer@gmail.com>
2019-05-29 06:15:33 +0200

    Added colors

---
+++ AndroidLogViewer/Style/Grouping.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace AndroidLogViewer.Style
+{
+    public class Grouping
+    {
+        public enum GroupingPosition
+        {
+            /// <summary>
+            /// Denotes that the element is not part of any grouping
+            /// </summary>
+            Single,
+
+            /// <summary>
+            /// Denotes that the element is the first inside its group
+            /// </summary>
+            First,
+
+            /// <summary>
+            /// Denotes that the element has predecessors and successors in its group
+            /// </summary>
+            Middle,
+
+            /// <summary>
+            /// Denotes that the element is the last inside its group
+            /// </summary>
+            Last
+        }
+
+        public static readonly DependencyProperty PositionProperty = DependencyProperty.RegisterAttached(
+            "Position", typeof(GroupingPosition), typeof(Grouping), new PropertyMetadata(default(GroupingPosition)));
+
+        public static void SetPosition(DependencyObject element, GroupingPosition value)
+        {
+            element.SetValue(PositionProperty, value);
+        }
+
+        public static GroupingPosition GetPosition(DependencyObject element)
+        {
+            return (GroupingPosition) element.GetValue(PositionProperty);
+        }
+    }
+}
